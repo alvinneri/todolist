ad1ea21fad639c662679100984ac3e5d35ea154f
Alvin <alvin.neri.ece@gamil.com>
2020-01-23 08:36:32 +0800

    to published in netlify

---
+++ src/components/dashboard.js
@@ -0,0 +1,109 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/dashboard.css';
+import { MDBInput } from "mdbreact";
+import { MDBListGroup, MDBListGroupItem, MDBContainer, MDBAnimation } from "mdbreact";
+
+class Dashboard extends Component {
+
+    constructor(props){
+        super(props);
+        this.state ={
+            todo : '',
+            priority : ''
+        }
+    }
+
+
+
+    onChangeTodo = (e) =>{
+        this.setState({
+            todo : e.target.value
+        })
+    }
+
+    onSubmitAdd = () => {
+       
+      const number = Math.floor(Math.random() * 20);
+
+      if(localStorage.getItem(number)){
+        localStorage.setItem(number+1 , this.state.todo )
+      }
+      else{
+        localStorage.setItem(number, this.state.todo )
+      }
+
+      this.setState({
+          todo : ''
+      })
+       
+    }
+
+    onSubmitDelete = () =>{
+        localStorage.clear();
+        this.setState({
+            todo: ''
+        })
+    }
+
+    getList = () => {
+        const list = [];
+        for (let i =0 ; i< Object.keys(localStorage).length ; i++){
+            const key = localStorage.key(i);            
+            const item = localStorage.getItem(key);
+            list.push({key : item });
+        }
+        return list
+    }
+
+    deleteItem = (key) =>{ 
+       
+        for (let i =0 ; i< Object.keys(localStorage).length ; i++){
+            const keyI = localStorage.key(i);
+            const item = localStorage.getItem(keyI);
+
+            if(key === item)
+           localStorage.removeItem(keyI);
+        }
+      
+        this.setState({
+            todo : ''
+        })
+    }
+
+    render() { 
+
+      let todoList = this.getList().map(todo =>
+        
+        <div>
+        <MDBAnimation type="bounceInLeft">
+        <MDBListGroupItem><button className="btn btn-light" id="listgroupbtn" onClick={()=>{ this.deleteItem(todo.key)}}>X</button>{todo.key}</MDBListGroupItem>
+        </MDBAnimation>
+        </div>
+        
+
+        )
+
+        return ( 
+            <div>
+                <div className="container mt-5 mx-auto ">
+                    <h1>TO-DO LIST</h1>
+                    <p>Developed by:<i> Alvin Neri</i></p>
+                    <MDBInput label="Add todo" id="input" name="todo" value={this.state.todo} onChange={this.onChangeTodo} />
+                    <button className="btn btn-light btncustom" onClick={this.onSubmitAdd}>ADD</button>
+                    <button className="btn btn-light btncustom" onClick={this.onSubmitDelete}>DELETE ALL</button>
+                </div>
+                <div className="container mt-2 mx-auto " id="list">
+                    <MDBContainer  className="mt-5 mb-5">
+                    <MDBListGroup>
+                    {todoList}
+                    </MDBListGroup>
+                    </MDBContainer>
+                   
+                </div>
+            </div>
+         );
+    }
+}
+ 
+export default Dashboard;
